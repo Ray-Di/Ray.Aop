03dbc7bc44ffad60ee7c5b18182e6fc340ae21bf
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-11-03 22:05:14 +0900

    Add detailed type annotations and improve doc comments
    Enhanced type annotations using Psalm for better static analysis and added detailed doc comments to clarify method functionalities. Also refined error handling and class instantiation assertions.

--- src/CompilerInterface.php
+++ src/CompilerInterface.php
@@ -4,22 +4,60 @@ declare(strict_types=1);
 
 namespace Ray\Aop;
 
+/**
+ * @psalm-import-type MethodInterceptors from Aspect
+ * @psalm-import-type MethodBindings from Aspect
+ * @psalm-import-type ClassBindings from Aspect
+ * @psalm-import-type MatcherConfig from Aspect
+ * @psalm-type AspectBind = array{
+ *   bind: Bind,
+ *   pointcut: array{
+ *     method: string,
+ *     interceptors: MethodInterceptors
+ *   }
+ * }
+ * @psalm-type BindCode = array{
+ *   interface: class-string,
+ *   name: string|null,
+ *   target: class-string|string|null,
+ *   scope: string|null,
+ *   bindWith: string,
+ *   type: 'to'|'toProvider'|'toInstance'|'toConstructor'|'toNull'
+ * }
+ * @psalm-type AspectCode = array{
+ *   target: class-string,
+ *   methods: MethodBindings
+ * }
+ * @psalm-type CompileResult = array{
+ *   binds: array<string, BindCode>,
+ *   aspectBinds: array<string, AspectCode>
+ * }
+ * @psalm-type ConstructorArguments = list<mixed>
+ */
 interface CompilerInterface
 {
     /**
-     * Compile class
+     * Compile dependency bindings and aspect bindings
      *
-     * @param class-string $class
+     *  Return class code which implements class-string<$class> with generated class name "{$class}_Generated_{$randomString}"
+     *
+     * @param class-string<T> $class Target class name
+     * @param BindInterface   $bind  Dependency binding
+     *
+     * @return class-string<T> Generated class name with interceptor weaved code
+     *
+     * @template T of object
      */
     public function compile(string $class, BindInterface $bind): string;
 
     /**
      * Return new instance weaved interceptor(s)
      *
-     * @param class-string<T> $class
-     * @param list<mixed>     $args
+     * @param class-string<T>      $class Target class name
+     * @param ConstructorArguments $args  Constructor arguments
+     * @param BindInterface        $bind  Dependency binding
      *
-     * @return T
+     * @return T New instance with woven interceptors
      *
      * @template T of object
      */
